0c3d12e9a839342c0925ac3cb719e23d26f04003
yessica <aria.801900@gmail.com>
2017-03-29 11:58:33 -0600

    ya sirven dos botones

--- trello.js
+++ trello.js
@@ -1,10 +1,29 @@
-function crearElemento (){
-		var nuevoElemento = document.getElementById("nuevoElemento").value;
-		if(nuevoElemento.length>0){
-			var elemento=document.createElement("li");
-			elemento.id=nuevoElemento;
-			elemento.innerHTML=nuevoElemento;
-			document.getElementById("lista1").appendChild(elemento);
-		}
+function crearElemento1 (){
+	var nuevoElemento = document.getElementById("nuevoElemento1").value;
+	if(nuevoElemento.length>0){
+		var elemento = document.createElement("li");
+		elemento.id = nuevoElemento;
+		elemento.innerHTML = nuevoElemento;
+		document.getElementById("lista1").appendChild(elemento);
+	}
 	return false;
+}
+
+function crearElemento2 (){
+	var nuevoElemento = document.getElementById("nuevoElemento2").value;
+	if(nuevoElemento.length>0){
+		var elemento = document.createElement("li");
+		elemento.id = nuevoElemento;
+		elemento.innerHTML = nuevoElemento;
+		document.getElementById("lista2").appendChild(elemento);
+	}
+	return false;
+}
+
+function crearLista () {
+	var lista = document.getElementById("nuevaLista").value;
+	document.getElementById("listaNueva").innerHTML = lista;
+	if(listaNueva.length>0){
+		
+	}
 }
