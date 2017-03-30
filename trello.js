748c4adf841e2ee6dbb08ddef66aad800e6f578b
yessica <aria.801900@gmail.com>
2017-03-30 16:13:46 -0600

    agrega lista

--- trello.js
+++ trello.js
@@ -1,42 +1,20 @@
 function crearElemento1 (){
 	var nuevoElemento = document.getElementById("nuevoElemento1").value;
-	if(nuevoElemento.length>0){
-		var elemento = document.createElement("li");
-		elemento.id = nuevoElemento;
-		elemento.innerHTML = nuevoElemento;
-		document.getElementById("lista1").appendChild(elemento);
-	}
-	return false;
-}
-
-function crearElemento2 (){
-	var nuevoElemento = document.getElementById("nuevoElemento2").value;
-	if(nuevoElemento.length>0){
-		var elemento = document.createElement("li");
-		elemento.id = nuevoElemento;
-		elemento.innerHTML = nuevoElemento;
-		document.getElementById("lista2").appendChild(elemento);
-	}
+	var elemento = document.createElement("li");
+	elemento.innerHTML = nuevoElemento;
+	document.getElementById("lista1").appendChild(elemento);
 	return false;
 }
 
 function crearLista () {
-	var lista = document.getElementById("nuevaLista").value;
-	document.getElementById("tituloNuevo").innerHTML = lista;	
-}
+	var lista = document.getElementById("titulo").value;
+	document.getElementById("tituloNuevo").innerHTML = lista;
+	var elemento = document.createElement("input");
+	var boton = document.createElement("input");
+	document.getElementById("nuevaLista").appendChild(elemento);
+	document.getElementById("nuevaLista").appendChild(boton);
 
-var elemen1 = document.getElementById("nuevoElemento3");
-
-function Lista (elmen1, elemen2, elemen3, elemen4,elemen5){
-	this.elmen1 = elmen1;
-	this.elmen2 = elmen2;
-	this.elmen3 = elmen3; 
-	this.elmen4 = elmen4;
-	this.elmen5 = elmen4;
-}
-
-var otraLista = new Lista (elemen1, elemen2, elemen3, elemen4, elemen5);
-
-function agregarLista (){
-	otraLista.;
+	elemento.type ="text";
+	boton.type ="button";
+	boton.value = "Crear pendiente";
 }
